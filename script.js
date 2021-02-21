d2d74ac01a1eb340819609306bab1824da852c49
groomyjohny <groomyjohny@gmail.com>
2021-02-21 13:13:17 +0500

    v.0.3: Мессенджер -> Сообщения

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         VK groomifier
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Removes pesky away.php wrapper for links in VKontakte social network.
+// @version      0.3
+// @description  Убирает away.vk.com и возращает "Сообщения" вместо "Мессенджера"
 // @author       groomyjohny
 // @match        https://*.vk.com/*
 // @grant        none
@@ -10,27 +10,28 @@
 
 const linkBeginning = "https://vk.com/away.php";
 const linkEnding = "&";
-(function() {
+let nextLinkId = 0;
+
+(setInterval( function() {
     'use strict';
 
-    console.log("Running from away.vk.com started");
+    console.log("VK groomifier started");
+
+    if (document.title == "Мессенджер") document.title = "Сообщения";
+    document.querySelectorAll('.left_label, inl_bl').forEach(el => {
+        if (el.innerHTML == "Мессенджер") el.innerHTML = "Сообщения";
+    });
+
     let links = document.getElementsByTagName("a");
     console.log("Found ",links.length," links on the page");
-    for (let i = 0; i < links.length; ++i)
+    for (let i = nextLinkId; i < links.length; ++i, ++nextLinkId)
     {
-        if (links[i].href.startsWith(linkBeginning))
+        let href = links[i].href;
+        if (href.startsWith(linkBeginning))
         {
-            let h = links[i].href;
-            console.log("Link ",i,h);
-            console.log("Links["+i+"] is spoofed by vk.com, fixing");
-            let s1 = h.substring(h.indexOf("to=")+3);
-            console.log("s1",s1);
-            let endIndex = s1.indexOf(linkEnding);
-            if (endIndex == -1) endIndex = s1.length;
-            let uri = s1.substring(0,endIndex);
-            console.log("Original URI:",uri);
-            console.log("Decoded URI: ",decodeURIComponent(uri));
-            links[i].href = decodeURIComponent(uri);
+            let url = new URL(href);
+            let searchParams = new URLSearchParams(url.search);
+            if (searchParams.has("to")) links[i].href = searchParams.get("to");
         }
     }
-})();
+},500));
